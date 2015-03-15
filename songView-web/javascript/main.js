dedf82dbf1073fdefbe493337246e122189c9240
David Johnson <fortjay81@gmail.com>
2015-03-14 23:10:41 -0700

    Summarizing Data to Bars

--- songView-web/javascript/main.js
+++ songView-web/javascript/main.js
@@ -8,6 +8,7 @@ function getTrack(){
     // request the track analysis
     $.getJSON(URL_ECHONEST_API + 'track/profile' + '?format=json&api_key='+API_KEY+'&id='+trackID+"&bucket=audio_summary",   
         function(data) {
+            console.log(data)
 
             // analysis returned to URL. Now we need to get the full detailed data
             analysisUrl = data.response.track.audio_summary.analysis_url;
@@ -21,15 +22,30 @@ function getTrack(){
 
 function generateSongView(songData){
 
+    // getting min/max timbre values for timbre normalization
+    var tMax = [-100, -100, -100];
+    var tMin = [100, 100, 100];
+    for(var i=0; i<songData.segments.length; i++){
+        var segment = songData.segments[i];
+
+        for(var j=0; j<3; j++){
+            t = segment.timbre[j];
+            if (t > tMax[j])
+                tMax[j] = t;
+            if (t <tMin[j])
+                tMin[j] = t;
+        }
+    }
+
     //SVG Width and height
     var w = 1400;
     var h = 100;
     var sidePadding = 10;
-    var bottomPadding = 10;
+    var bottomPadding = 25;
 
     //Individual Pitch Box Dimensions
-    var rectW = 1;
-    var rectH = 4;
+    var rectW = 3;
+    var rectH = 3;
     var pitchPadding = 1;  // Vertical Padding
 
     // Loudness Dimensions (uses width of Pitch boxes)
@@ -51,53 +67,154 @@ function generateSongView(songData){
                 .attr("height", h);
    
 
+    // initialize counter and accumulators to 0
+    var barIdx = 0;
+    var segIdx= 0
+    var numSegments = 0;
+    var pitchTots = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var loudnessTot = 0
+    var timbreTots = [0, 0, 0]
 
-    // For all segements
-    for(var idx=0; idx<songData.segments.length; idx++){
+    for(; barIdx<songData.bars.length; segIdx++){      // For all bars
 
-        // UPDATE to summarize the data
-        // Draw the 12 boxes representing pitch
-        svg.selectAll("rect"+idx)
-            .data(songData.segments[idx].pitches)
-            .enter()
-            .append("rect")
-            .attr("x", function(d){
-                return sidePadding + (idx * rectW);
-            })
-            .attr("y", function(d,i){
-                return h - bottomPadding - (i * (rectH + pitchPadding));
-            })
-            .attr("width", rectW)
-            .attr("height", rectH)
-            .attr("fill", "teal")
-            .attr("fill-opacity", function(d){
-                return d;
-            });
+        // Get necessary data
+        bar = songData.bars[barIdx];
+        barEndTime = bar.start + bar.duration;
+        segment = songData.segments[segIdx];
 
-        // Draw the box representing loudness
-        svg.selectAll("loudRect"+idx)
-            .data([(songData.segments[idx].loudness_max + songData.segments[idx].loudness_start)/2])
-            .enter()
-            .append("rect")
-            .attr("x", function(d){
-                return sidePadding + (idx * rectW);
-            })
-            .attr("y", function(d){
-                height = loudnessScale(d);
-                return h - bottomPadding - (13 * (rectH + pitchPadding) - loudnessPadding) - height;
-            })
-            .attr("width", rectW)
-            .attr("height", function(d){
-                return loudnessScale(d);
-            })
-            .attr("class", "loudness");
+        if (segment.start < barEndTime){
+            // if next segment is within current bar
+            // summarize the data
 
-        lineY = h - bottomPadding - (13 * (rectH + pitchPadding) - loudnessPadding) - loudnessMaxH
-        svg.append("line")
-            .attr("x1", sidePadding)
-            .attr("y1", lineY)
-            .attr("x2", songData.segments.length * rectW + sidePadding)
-            .attr("y2", lineY)
-            .attr("class", "loudness");
-    };
+            for(var i=0;i<12;i++){  //update pitch totals for all 12 pitches
+                pitchTots[i]+=segment.pitches[i]
+            }
+            loudnessTot += (segment.loudness_start + segment.loudness_max) / 2;  // get average loudness per segment
+            timbreTots[0] = segment.timbre[0];
+            timbreTots[1] = segment.timbre[1];
+            timbreTots[2] = segment.timbre[2];
+            numSegments +=1;
+        }
+        else{
+            pitchAvg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            timbreAvg = [0, 0, 0]
+            //generate averages of data
+            for(var i=0;i<12;i++){
+                pitchAvg[i] = pitchTots[i] / numSegments;
+                pitchTots[i] = 0;  // done with value reset to 0
+            }
+
+            loudnessAvg = loudnessTot / numSegments;
+            loudnessTot = 0  // done with total loud so reset it
+
+            timbreAvg[0] = timbreTots[0] / numSegments;
+            timbreAvg[1] = timbreTots[1] / numSegments;
+            timbreAvg[2] = timbreTots[2] / numSegments;
+
+            // done with timbre tots reset to 0
+            timbreTots[0] = 0;
+            timbreTots[1] = 0;
+            timbreTots[2] = 0;
+
+            numSegments = 0; // done with summarization reset numSegments
+            barIdx += 1;     // and update bar idx
+
+
+            // Generate color
+            var rgbVals =  [0, 0, 0];
+            for (var i=0; i<3; i++){
+                t = timbreAvg[i];
+                if (t < tMin[i])
+                    // tNorm = (tMin[i] - t) / (tMax[i] - tMin[i]);
+                    tNorm = t/tMax[i];
+                else
+                    //tNorm = (t - tMin[i]) / (tMax[i] - tMin[i]);
+                    tNorm = t/tMax[i];
+                
+                rgbVals[i] = tNorm;
+                console.log(rgbVals)
+            }
+
+            // Now Draw the 12 boxes representing pitch
+            svg.selectAll("rect"+barIdx)
+                .data(pitchAvg)
+                .enter()
+                .append("rect")
+                .attr("x", function(d){
+                    return sidePadding + ((barIdx-1) * rectW);
+                })
+                .attr("y", function(d,i){
+                    return h - bottomPadding - (i * (rectH + pitchPadding));
+                })
+                .attr("width", rectW)
+                .attr("height", rectH)
+                .attr("fill", function(d){
+                    return d3.rgb(rgbVals[0] * 360, rgbVals[1], rgbVals[2]);
+                })
+                .attr("fill-opacity", function(d){
+                    return d;
+                });
+
+            // Draw the box representing loudness
+            svg.selectAll("loudRect"+barIdx)
+                .data([loudnessAvg])
+                .enter()
+                .append("rect")
+                .attr("x", function(d){
+                    return sidePadding + ((barIdx-1) * rectW);
+                })
+                .attr("y", function(d){
+                    height = loudnessScale(d);
+                    return h - bottomPadding - (13 * (rectH + pitchPadding) - loudnessPadding) - height;
+                })
+                .attr("width", rectW)
+                .attr("height", function(d){
+                    return loudnessScale(d);
+                })
+                .attr("fill", function(d){
+                    return d3.rgb(rgbVals[0], rgbVals[1], rgbVals[2]);
+                });
+
+            // // Draw the box representing loudness
+            // svg.selectAll("loudRect2"+barIdx)
+            //     .data([loudnessAvg])
+            //     .enter()
+            //     .append("rect")
+            //     .attr("x", function(d){
+            //         return sidePadding + ((barIdx-1) * rectW);
+            //     })
+            //     .attr("y", function(d){
+            //         height = loudnessScale(d);
+            //         return h - loudnessMaxH - bottomPadding + height;
+            //     })
+            //     .attr("width", rectW)
+            //     .attr("height", function(d){
+            //         return loudnessScale(d);
+            //     })
+            //     .attr("fill", function(d){
+            //         return d3.rgb(rgbVals[0], rgbVals[1], rgbVals[2]);
+            //     });
+
+
+            // lineY = h - bottomPadding - (13 * (rectH + pitchPadding) - loudnessPadding) - loudnessMaxH
+            // svg.append("line")
+            //     .attr("x1", sidePadding)
+            //     .attr("y1", lineY)
+            //     .attr("x2", songData.bars.length * rectW + sidePadding)
+            //     .attr("y2", lineY)
+            //     .attr("stroke", function(d){
+            //         return d3.rgb(150, 150, 150);
+            //     });
+
+            // Done drawing boxes now store segment info for next bar...
+            for(var i=0;i<12;i++){  //update pitch totals for all 12 pitches
+                pitchTots[i]+=segment.pitches[i]
+            }
+            loudnessTot += (segment.loudness_start + segment.loudness_max) / 2;  // get average loudness per segment
+            timbreTots[0] = segment.timbre[0];
+            timbreTots[1] = segment.timbre[1];
+            timbreTots[2] = segment.timbre[2];
+            numSegments +=1;
+        }
+    }
 }
